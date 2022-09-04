06c8247be47e24dcec1a8862e51b0342605d809a
soukaisucc <thainguyen1@hotmail.com>
2022-09-04 16:01:06 +1200

    Made some changes to the code for visibility

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computeScore = 0;
+
 let computerOptions = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
@@ -10,20 +13,20 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-   computerSelection = getComputerChoice();
    playerSelection = prompt("Will you select Rock, Paper, or Scissors?");
+   computerSelection = getComputerChoice();
 
-   if (playerSelection.toLowerCase() == computerSelection) {
+   if (playerSelection.toLowerCase() === computerSelection) {
       return "Tie";
    } else if (
-      (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors")  || 
-      (playerSelection.toLowerCase() == "paper" && computerSelection == "rock")  || 
-      (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
+      (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors")  || 
+      (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")  || 
+      (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")
    ) {
-      return "Rare player W";
+      return "player W";
       ++playerScore;
    } else {
-      return "Common player L, plus one to the computer";
+      return "computer W";
       ++computeScore;
    }
 
@@ -40,8 +43,5 @@ function game () {
    } else {
       return "Common computer victory";
    }
-
-   let playerScore = 0;
-   let computeScore = 0;
 
 }
