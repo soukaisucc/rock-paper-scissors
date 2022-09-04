106f082cb804882d7a10ca8e87d419981f2deac7
soukaisucc <thainguyen1@hotmail.com>
2022-09-04 15:25:40 +1200

    Finished writing the script of the game

--- rps.js
+++ rps.js
@@ -10,8 +10,38 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
+   computerSelection = getComputerChoice();
+   playerSelection = prompt("Will you select Rock, Paper, or Scissors?");
+
+   if (playerSelection.toLowerCase() == computerSelection) {
+      return "Tie";
+   } else if (
+      (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors")  || 
+      (playerSelection.toLowerCase() == "paper" && computerSelection == "rock")  || 
+      (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper")
+   ) {
+      return "Rare player W";
+      ++playerScore;
+   } else {
+      return "Common player L, plus one to the computer";
+      ++computeScore;
+   }
+
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game () {
+
+   for (let i = 0; i < 5; i++) {
+      console.log (playRound());
+   }
+
+   if (playerScore > computeScore) {
+      return "Rare player victory";
+   } else {
+      return "Common computer victory";
+   }
+
+   let playerScore = 0;
+   let computeScore = 0;
+
+}
